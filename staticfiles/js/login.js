00ea5eb77b8f02f27b92312c7de7569c257958f7
Sajad Aslan <aanoarmuradi@gmail.com>
2026-04-20 11:34:59 +0430

    fix static files

---
+++ staticfiles/js/login.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const form = document.querySelector("#loginForm");
+
+    if (!form) {
+        console.error("Form not found!");
+        return;
+    }
+
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        console.log("clicked");
+
+        const username = document.querySelector("#username").value;
+        const password = document.querySelector("#password").value;
+
+        try {
+            const response = await fetch("http://127.0.0.1:8000/api/token/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ username, password })
+            });
+            const data = await response.json();
+
+            console.log(data);
+
+            if (response.ok) {
+                localStorage.setItem("access", data.access);
+                const token = localStorage.getItem("access");
+
+                const Response = await fetch("http://127.0.0.1:8000/users/user/info/", {
+                    headers: {
+                        "Authorization": `Bearer ${token}`
+            }
+            });
+            const Data = await Response.json();
+            if (Data.role === "user") {
+                window.location.href = "/shop/dashboard/";
+
+            }
+             else {
+                window.location.href = "/dashboard/";
+            }
+
+            }
+           
+        } catch (err) {
+            console.error(err);
+        }
+    });
+});
