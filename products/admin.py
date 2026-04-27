9acd41c7c2365f50fdfad7738ab4fdc275441e90
Sajad Aslan <aanoarmuradi@gmail.com>
2026-04-27 11:09:39 +0430

    update

--- products/admin.py
+++ products/admin.py
@@ -9,29 +9,37 @@ SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 
-class ProductAdmin(admin.ModelAdmin):
+def save_model(self, request, obj, form, change):
+   
+    file = request.FILES.get('image')
 
-    def save_model(self, request, obj, form, change):
-        file = request.FILES.get('image')
+    print("FILE:", file)
 
-        if file:
-            file_name = file.name
+    if file:
+        file_name = file.name
 
-            upload_url = f"{SUPABASE_URL}/storage/v1/object/media/{file_name}"
+        SUPABASE_URL = os.getenv("SUPABASE_URL")
+        SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-            headers = {
-                "Authorization": f"Bearer {SUPABASE_KEY}",
-                "Content-Type": file.content_type
-            }
+        print("URL:", SUPABASE_URL)
+        print("KEY:", SUPABASE_KEY)
 
-            response = requests.post(upload_url, headers=headers, data=file.read())
+        upload_url = f"{SUPABASE_URL}/storage/v1/object/media/{file_name}"
 
-            if response.status_code == 200:
-                obj.image_url = f"{SUPABASE_URL}/storage/v1/object/public/media/{file_name}"
-            else:
-                print("Upload Error:", response.text)
+        headers = {
+            "Authorization": f"Bearer {SUPABASE_KEY}",
+            "Content-Type": file.content_type
+        }
 
-        super().save_model(request, obj, form, change)
+        response = requests.post(upload_url, headers=headers, data=file.read())
+
+        print("STATUS:", response.status_code)
+        print("RESPONSE:", response.text)
+
+        if response.status_code == 200:
+            obj.image_url = f"{SUPABASE_URL}/storage/v1/object/public/media/{file_name}"
+
+    super().save_model(request, obj, form, change)
 
 
 admin.site.register(Product, ProductAdmin)
