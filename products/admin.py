6f75cc68f81ddef08babccaf3ba002796cba779e
Sajad Aslan <aanoarmuradi@gmail.com>
2026-04-27 23:44:26 +0430

    test admin

--- products/admin.py
+++ products/admin.py
@@ -5,38 +5,10 @@ import os
 
 
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-
-
-
 class ProductAdmin(admin.ModelAdmin):
 
-   class ProductAdmin(admin.ModelAdmin):
-
     def save_model(self, request, obj, form, change):
-        import requests
-        import os
-
-        file = request.FILES.get('image')
-
-        if file:
-            file_name = file.name
-
-            SUPABASE_URL = os.getenv("SUPABASE_URL")
-            SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-
-            upload_url = f"{SUPABASE_URL}/storage/v1/object/media/{file_name}"
-
-            headers = {
-                "Authorization": f"Bearer {SUPABASE_KEY}",
-                "Content-Type": file.content_type
-            }
-
-            response = requests.post(upload_url, headers=headers, data=file.read())
-
-            if response.status_code in [200, 201]:
-                obj.image_url = f"{SUPABASE_URL}/storage/v1/object/public/media/{file_name}"
+        print("🔥 SAVE_MODEL CALLED 🔥")
 
         super().save_model(request, obj, form, change)
 
