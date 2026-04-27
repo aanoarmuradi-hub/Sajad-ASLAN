82f71104b1d02a7639d1b730b093e54ef3eb858e
Sajad Aslan <aanoarmuradi@gmail.com>
2026-04-27 11:17:10 +0430

    update

--- products/admin.py
+++ products/admin.py
@@ -9,37 +9,39 @@ SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 
-def save_model(self, request, obj, form, change):
-   
-    file = request.FILES.get('image')
 
-    print("FILE:", file)
+class ProductAdmin(admin.ModelAdmin):
 
-    if file:
-        file_name = file.name
+    def save_model(self, request, obj, form, change):
+        file = request.FILES.get('image')
 
-        SUPABASE_URL = os.getenv("SUPABASE_URL")
-        SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+        print("FILE:", file)
 
-        print("URL:", SUPABASE_URL)
-        print("KEY:", SUPABASE_KEY)
+        if file:
+            file_name = file.name
 
-        upload_url = f"{SUPABASE_URL}/storage/v1/object/media/{file_name}"
+            SUPABASE_URL = os.getenv("SUPABASE_URL")
+            SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-        headers = {
-            "Authorization": f"Bearer {SUPABASE_KEY}",
-            "Content-Type": file.content_type
-        }
+            print("URL:", SUPABASE_URL)
+            print("KEY:", SUPABASE_KEY)
 
-        response = requests.post(upload_url, headers=headers, data=file.read())
+            upload_url = f"{SUPABASE_URL}/storage/v1/object/media/{file_name}"
 
-        print("STATUS:", response.status_code)
-        print("RESPONSE:", response.text)
+            headers = {
+                "Authorization": f"Bearer {SUPABASE_KEY}",
+                "Content-Type": file.content_type
+            }
 
-        if response.status_code == 200:
-            obj.image_url = f"{SUPABASE_URL}/storage/v1/object/public/media/{file_name}"
+            response = requests.post(upload_url, headers=headers, data=file.read())
 
-    super().save_model(request, obj, form, change)
+            print("STATUS:", response.status_code)
+            print("RESPONSE:", response.text)
+
+            if response.status_code == 200:
+                obj.image_url = f"{SUPABASE_URL}/storage/v1/object/public/media/{file_name}"
+
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(Product, ProductAdmin)
