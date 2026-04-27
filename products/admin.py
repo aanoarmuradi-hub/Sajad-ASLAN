6c56226a9dc9c09ef6a03902456d086d4ed5a46b
Sajad Aslan <aanoarmuradi@gmail.com>
2026-04-27 23:53:28 +0430

    force ProductAdmin

--- products/admin.py
+++ products/admin.py
@@ -3,13 +3,34 @@ from .models import Product
 import requests
 import os
 
-
-
 class ProductAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         print("🔥 SAVE_MODEL CALLED 🔥")
 
+        file = request.FILES.get('image')
+
+        if file:
+            file_name = file.name
+
+            SUPABASE_URL = os.getenv("SUPABASE_URL")
+            SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+            upload_url = f"{SUPABASE_URL}/storage/v1/object/media/{file_name}"
+
+            headers = {
+                "Authorization": f"Bearer {SUPABASE_KEY}",
+                "Content-Type": file.content_type
+            }
+
+            response = requests.post(upload_url, headers=headers, data=file.read())
+
+            if response.status_code in [200, 201]:
+                obj.image_url = f"{SUPABASE_URL}/storage/v1/object/public/media/{file_name}"
+
         super().save_model(request, obj, form, change)
 
+
+# 💥 مهم‌ترین خط:
+admin.site.unregister(Product)
 admin.site.register(Product, ProductAdmin)
